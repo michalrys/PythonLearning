f6bb40bb7159f924bd4b530003eced8ac2d634fa
michalrys <39671742+michalrys@users.noreply.github.com>
2018-06-04 19:29:35 +0200

    add zadanie1

---
+++ DAY03_2018-06-04/Day03_task01.py
@@ -0,0 +1,22 @@
+# 2018-06-04
+# Michal Rys
+# DAY 03: task01
+
+# 1 - podaj imie użytkownika (input)
+# 2.0 - jeśli imie podane przez użytkownika == Piotr ?
+# 2.1 - wyświetl tekst powitalny
+# 2.2 - w przeciwnym razie --> wyświetl tekst: "Hej, poklikasz...?"
+ADMIN_USERNAME = 'Piotr'
+
+input_user_name = input('Podaj imie: ')
+
+# obróbka danych wejściowych
+user_name = input_user_name.lower()
+user_name = user_name.capitalize()
+
+#               było tutaj 'Piotr' --> RMB --> Refactor -> Exctract -> Constant
+if user_name == ADMIN_USERNAME:
+    # można komunikaty wyrzucić też do stałych = czytelniejsze, łatw. do rozsz.
+    print(f'Witaj {user_name}')
+else:
+    print('Hej, poklikasz... ?')
