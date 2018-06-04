a029ed1ff2a87e206c6d21b494f131b1d017a5a0
michalrys <39671742+michalrys@users.noreply.github.com>
2018-06-04 20:19:04 +0200

    new task check wheter value is even or odd

--- DAY03_2018-06-04/Day03_task02.py
+++ DAY03_2018-06-04/Day03_task02.py
@@ -2,22 +2,14 @@
 # Michal Rys
 # DAY 03: task02
 
-# 1 - podaj imie użytkownika (input)
-# 2.0 - jeśli imie podane przez użytkownika == Piotr ?
-# 2.1 - wyświetl tekst powitalny
-# 2.2 - w przeciwnym razie --> wyświetl tekst: "Hej, poklikasz...?"
-ADMIN_USERNAME = 'Piotr'
+# TODO: napisz program, który sprawdzi czy liczba jest parzysta.
+# TODO: liczba do sprawdzenia jest podawana przez użytkownika
 
-input_user_name = input('Podaj imie: ')
+value = int(input('Podaj liczbe: '))
 
-# obróbka danych wejściowych
-# patrz do method / function chaining
-user_name = input_user_name.lower().capitalize()
-
-#               było tutaj 'Piotr' --> RMB --> Refactor -> Exctract -> Constant
-if user_name == ADMIN_USERNAME:
-    # można komunikaty wyrzucić też do stałych = czytelniejsze, łatw. do rozsz.
-    print(f'Witaj {user_name}')
+if value % 2:
+    print(f'Podana liczba: {value} jest nieprzysta')
 else:
-    print('Hej, poklikasz... ?')
-# test
+    print(f'Podana liczba: {value} jest przysta')
+
+# sprawdź ten kod : http://pythontutor.com/visualize.html#mode=edit
