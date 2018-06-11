45ca6e21beb8d85e2c8e35f97c0afb6bb5027db6
michalrys <39671742+michalrys@users.noreply.github.com>
2018-06-11 18:27:53 +0200

    notes from lesson.

---
+++ DAY05_2018-06-11/!_Day05_notes_from_lesson1.py
@@ -0,0 +1,25 @@
+# 2018-06-11
+# InfoShare: day 05. Notatki z zajęć.
+# M. Ryś
+
+# LISTY
+
+# zawiera intiger
+lista1 = [1 ,2, 3]
+
+# może być dowolny typ danych
+lista2 = ['drzewo', 'doniczka', 'ziemia']
+lsita3 = []
+lista4 = list(range(2,5))
+
+# listy zagnieżdżone / macierz / tablica dwuwymiarowa
+
+lista11 = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ]
+lista12 = [[1, 2, 3],
+           [4, 5, 6],
+           [7, 8, 9]]
+
+print(lista12)
+lista12[1][2] # >> 6
+
+#
