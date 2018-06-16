60a4d131c7e3f8481584b17cd85c360528411687
michalrys <39671742+michalrys@users.noreply.github.com>
2018-06-16 09:43:15 +0200

    ???

---
+++ DAY05_2018-06-11/HW_lists/Day5_LISTS_HW_07_reszta_z_monent_SOL01_MR.py
@@ -0,0 +1,9 @@
+# 2018-06-14
+# Autor: Michał Ryś
+# InfoShare: day 05: listy. Homework
+
+# program wydający resztę z podanych monet []
+# ------------------------------------------------------------------------------
+
+# constants
+# TODO: o co chodzi?
